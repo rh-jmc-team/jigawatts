017570c074e659df021a4a44977842e3f3b6cfd4
Jiri Vanek <jvanek@redhat.com>
2021-08-20 17:33:35 +0200

    Moved library loading code to diferent class

--- src/main/java/com/redhat/jigawatts/Jigawatts.java
+++ src/main/java/com/redhat/jigawatts/Jigawatts.java
@@ -38,7 +38,6 @@
 
 package com.redhat.jigawatts;
 
-import java.net.URL;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
@@ -48,7 +47,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.InputStream;
 import java.nio.file.Paths;
 import java.util.Collections;
 
@@ -151,137 +149,14 @@ public class Jigawatts {
         return Collections.unmodifiableList(crContext.restoreHooks);
     }
 
-    private static URL getInternalLibrary() {
-        String libraryName = System.mapLibraryName("Jigawatts");
-        return Jigawatts.class.getClassLoader().getResource(libraryName);
-    }
-
-    private static InputStream getInternalLibraryStream() throws IOException {
-        return getInternalLibrary().openStream();
-    }
-
-    private static void copyLibrary(String library, File desFile) throws IOException {
-        jigaLog("Extracting internal libray to " + desFile.getAbsolutePath());
-        jigaLog("Interal library is: " + getInternalLibrary().toExternalForm());
-        try (InputStream is = getInternalLibraryStream();
-             FileOutputStream os = new FileOutputStream(desFile)) {
-            byte[] buf = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = is.read(buf)) > 0) {
-                os.write(buf, 0, bytesRead);
-            }
-        }
-    }
-
-    private static void jigaLog(String s) {
-        if (getVerbose()) {
-            System.err.println(s);
-        }
-    }
-
-    private static String getPropertyOrVar(String s) {
-        String prop = System.getProperty(s);
-        if (prop != null) {
-            return trimToNull(prop);
-        }
-        return trimToNull(System.getenv(s.toUpperCase().replace(".", "_")));
-    }
-
-    private static String trimToNull(String s) {
-        if (s == null) {
-            return null;
-        }
-        s = s.trim();
-        if (s.isEmpty()) {
-            return null;
-        }
-        return s;
-    }
-
-    private static boolean getVerbose() {
-        return "true".equalsIgnoreCase(getPropertyOrVar(VERBOSE_PROP));
-    }
-
-    private static String getInternalLibraryExtractedFile() {
-        return getPropertyOrVar(LIBRARY_TARGETFILE_PROP);
-    }
-
-    private static String getExternalLibraryFile() {
-        return getPropertyOrVar(LIBRARY_EXTERNAL_PROP);
-    }
-
-    private static void loadInJarLibrary() {
-        File tmpLibrary = null;
-        try {
-            if (getInternalLibraryExtractedFile() == null) {
-                tmpLibrary = File.createTempFile("jigawatts", ".so");
-            } else {
-                tmpLibrary = new File(getInternalLibraryExtractedFile());
-            }
-            if (!(tmpLibrary.exists() && tmpLibrary.length() > 0)) {
-                copyLibrary("Jigawatts", tmpLibrary);
-            }
-        } catch (IOException ex) {
-            throw new RuntimeException("can't initialize Jigawatts", ex);
-        }
-        jigaLog("Loading internal libray from " + tmpLibrary.getAbsolutePath());
-        System.load(tmpLibrary.getAbsolutePath());
-    }
-
-    private static void loadSystemLib() {
-        String lib = System.mapLibraryName("Jigawatts");
-        jigaLog("Loading system library " + lib);
-        for (String path : System.getProperty("java.library.path").split(":")) {
-            try {
-                String libPath = path + File.separator + lib;
-                System.load(libPath);
-                jigaLog("Loaded system library: " + libPath);
-                return;
-            } catch (UnsatisfiedLinkError e) {
-                continue;
-            }
-        }
-        throw new RuntimeException(lib + " library not found on java.library.path/LD_LIBRARY_PATH!");
-    }
-
-    private static final String LIBRARY_TARGETFILE_PROP = "jigawatts.library.targetfile";
-    private static final String LIBRARY_EXTERNAL_PROP = "jigawatts.library";
-    private static final String VERBOSE_PROP = "jigawatts.verbose";
-    private static final String SYSTEM_LIB_SWITCH = "SYSTEM";
-
     static {
-        if (getInternalLibrary() == null) {
-            jigaLog("Internal library missing!");
-            if (getExternalLibraryFile() == null) {
-                loadSystemLib();
-            } else {
-                System.load(getExternalLibraryFile());
-                jigaLog("Loaded custom library: " + getExternalLibraryFile());
-            }
-        } else {
-            jigaLog("Internal library present");
-            if (getExternalLibraryFile() == null) {
-                loadInJarLibrary();
-            } else if (SYSTEM_LIB_SWITCH.equals(getExternalLibraryFile())) {
-                loadSystemLib();
-            } else {
-                jigaLog("Loading custom library: " + getExternalLibraryFile());
-                System.load(getExternalLibraryFile());
-            }
-        }
-        System.loadLibrary("criu");
+        LibraryLoader.loadLibrary();
         checkpointHooks = new ArrayList<Hook>();
         restoreHooks = new ArrayList<Hook>();
         crContext = new Jigawatts();
     }
 
-
     public static void main(String... args) {
-        System.out.println("Native library loaded!");
-        System.out.println(LIBRARY_TARGETFILE_PROP + ": " + getInternalLibraryExtractedFile());
-        System.out.println(LIBRARY_EXTERNAL_PROP + ": " + getExternalLibraryFile());
-        System.out.println("You may  use " + SYSTEM_LIB_SWITCH + " to force search of system library with internal library present");
-        System.out.println(VERBOSE_PROP + ": " + getVerbose());
+        LibraryLoader.sayHello();
     }
-
 }
